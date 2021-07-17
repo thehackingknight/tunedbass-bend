29827600499b5609928257e0894f74d7593efa03
TheHackingKnight <77808901+thehackingknight@users.noreply.github.com>
2021-07-18 01:55:55 +0200

    dbgng cors003

--- routes/upload.js
+++ routes/upload.js
@@ -58,7 +58,7 @@ const upload = multer({
 
 
 router.post("/", (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
   upload(req, res, err=>{
     if (err instanceof multer.MulterError) {
       // A Multer error occurred when uploading.
