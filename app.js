7feff037b06be8039c52761e2d405b186ae1672a
TheHackingKnight <77808901+thehackingknight@users.noreply.github.com>
2021-07-20 00:15:50 +0200

    cors debug001

--- app.js
+++ app.js
@@ -13,7 +13,13 @@ var downloadRouter = require('./routes/download');
 const cors = require('cors');
 
 var app = express();
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
